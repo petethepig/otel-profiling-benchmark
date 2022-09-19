78382f18ee9458d09c21e55d41c9e87f541f2f00
Dmitry Filimonov <dmitry@pyroscope.io>
2022-09-18 23:03:03 -0700

    chore: setting up the skeleton

--- cmd/convert/main.go
+++ cmd/convert/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+)
 
 func main() {
-	fmt.Println("TODO: implement")
+	filepaths, err := filepath.Glob("./profiles/src/*")
+
+	if err != nil {
+		panic(err)
+	}
+
+	for _, path := range filepaths {
+		if strings.HasSuffix(path, ".collapsed") {
+			copyFile(path, strings.Replace(path, "src", "intermediary", 1))
+		} else {
+			panic("Unexpected file extension")
+		}
+	}
+}
+
+func copyFile(src, dst string) error {
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, in)
+	if err != nil {
+		return err
+	}
+	return out.Close()
 }
--- reference/profile.go
+++ reference/profile.go
@@ -1,5 +1,29 @@
 package reference
 
+import (
+	"encoding/json"
+	"io"
+)
+
+type line struct {
+	Stacktrace string
+	Value      int
+}
+
 type Profile struct {
-	lines []string
+	Lines []line
+}
+
+func New() *Profile {
+	return &Profile{}
+}
+
+func (p *Profile) Append(stacktrace string, value int) {
+	p.Lines = append(p.Lines, line{Stacktrace: stacktrace, Value: value})
+}
+
+func (p *Profile) Serialize(w io.Writer) error {
+
+	e := json.NewEncoder(w)
+	return e.Encode(p)
 }
